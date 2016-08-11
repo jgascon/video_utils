195088c0657f938e7c49d349641c1e17d6349da7
jgascon <jorge.gascon.perez@gmail.com>
2016-08-12 00:59:07 +0200

    Videos project generator finished.
    Next Step: Make a video.

--- video_project_generator.py
+++ video_project_generator.py
@@ -5,9 +5,37 @@ import sys
 import VideoUtils as vu
 
 
+#--------------------- Constants and Global variables ------------------------
+
 PROJECT_NAME = sys.argv[1]
 PROJECT_VIDEOS_PATH = sys.argv[2]
+if PROJECT_VIDEOS_PATH[-1] != "/":
+    PROJECT_VIDEOS_PATH += "/"
+
 PROJECT_VIDEO_TEMPLATE = "_video_project_template.py_"
+
+
+
+#-------------------------- Auxiliar Functions -------------------------------
+
+
+def create_symlink(directory, symlink_name):
+    if os.path.exists(symlink_name):
+        os.remove(symlink_name)
+    os.symlink(directory, symlink_name)
+
+
+
+def create_folder_and_symlink(symlink_name):
+    directory = PROJECT_VIDEOS_PATH + symlink_name
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    create_symlink(directory, symlink_name)
+
+
+
+
+#---------------------------- Main Program -----------------------------------
 
 
 print("\n---------------- Video Project Generator ----------------\n")
@@ -21,23 +49,11 @@ print("  Videos Path:  [" + PROJECT_VIDEOS_PATH + "]\n")
 videos_filenames = vu.get_list_of_videos_filenames_of(PROJECT_VIDEOS_PATH)
 clips_filenames_str = "videos_clips = [\n"
 
-#~ print(" Videos Found: ")
-
 for video_filename in videos_filenames:
-    video_filename = video_filename.replace(PROJECT_VIDEOS_PATH, "videos")
-    #~ print("    " + video_filename)
-    clips_filenames_str += '                ["'+video_filename+'", "00:00:00", "00:00:00"],\n'
-    #~ clips_filenames += [[]]
+    video_filename = video_filename.replace(PROJECT_VIDEOS_PATH, "videos/")
+    clips_filenames_str += '               ["'+video_filename+'", "00:00:00", "00:00:00"],\n'
 
 clips_filenames_str += "               ]\n"
-
-#~ print("")
-
-
-#~ for clip_filename in clips_filenames:
-    #~ print("    ", clip_filename)
-
-#~ print("")
 
 
 
@@ -50,10 +66,16 @@ template = template.replace("%PROJECT_NAME%", PROJECT_NAME)
 template = template.replace("%LIST_OF_VIDEOS_CLIPS%", clips_filenames_str)
 
 
-
 # Saving the project filename.
 f = open(PROJECT_NAME+".py", "w")
 f.write(template)
 f.close()
 
 os.system("chmod a+x " + PROJECT_NAME + ".py")
+
+
+# Creating the folders and the symbolic links
+create_symlink(PROJECT_VIDEOS_PATH, "videos")
+create_folder_and_symlink("temp_clips")
+create_folder_and_symlink("sounds")
+create_folder_and_symlink("finals")
